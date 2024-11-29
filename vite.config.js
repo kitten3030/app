6db7919542d1cca36d250be343f06eaded503393
kitten3030 <61309541+kitten3030@users.noreply.github.com>
2024-11-29 09:27:06 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
